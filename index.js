a8f668f7b18d65d2700a0c40406b2af363153c89
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-19 11:16:20 +0530

    Update index.js
     Color Commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 app=express();
 
+bgcolor="yellow"
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 
 app.get("/",function(request,response){
-    response.send("The application is running very very perfectly")
+    response.send('<h1 style="background-color:'+bgcolor+';">The application is running very very perfectly</h1>')
 })
-app.listen(port,ip);
+app.listen(port,ip,function(){
+      let randomcolors =["blue","red","green","yellow"];
+    
+       bgcolor= randomcolors[Math.round(Math.random()*3)]
+});
 console.log("app is now actually listening on %s port and %s ",ip,port)
